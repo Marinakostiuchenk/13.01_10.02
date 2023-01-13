1bc66c6f306a1a50f381fa8740a994ee3b4a849a
Marina <business.voitishina@yandex.ru>
2023-01-13 21:03:06 +0300

    Added homework8

---
+++ homework8/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Напишите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N % 2 == 0)
+{
+    while (N > 0)
+    {
+        Console.Write($"{N} ");
+        N -= 2;
+    }
+}
+else
+{
+    N = N - 1;
+    while (N > 0)
+    {
+        Console.Write($"{N} ");
+        N -= 2;
+    }
+}
