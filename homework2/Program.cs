4b6cd69d0a4b2dbf316893f00799c7279cc2975c
Marina <business.voitishina@yandex.ru>
2023-01-13 15:09:36 +0300

    Added homework2

---
+++ homework2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число: ");
+int first_number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int second_number = Convert.ToInt32(Console.ReadLine());
+
+if (first_number > second_number)
+{
+    Console.WriteLine($"max = {first_number} ");
+}
+else
+{
+    Console.WriteLine($"max = {second_number} ");
+}
