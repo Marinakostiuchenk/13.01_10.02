87e0563ff625a1e01685eb52dcbb26c022772a34
Marina <business.voitishina@yandex.ru>
2023-01-13 15:35:50 +0300

    Added homework4

---
+++ homework4/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+    if (number1 > number3)
+    {
+        Console.WriteLine(number1);
+    }
+    else if (number1 < number3)
+    {
+        Console.WriteLine(number3);
+    }
+}
+else if (number1 < number2)
+{
+    if (number2 > number3)
+    {
+        Console.WriteLine(number2);
+    }
+    else if (number2 < number3)
+    {
+        Console.WriteLine(number3);
+    }
+}
