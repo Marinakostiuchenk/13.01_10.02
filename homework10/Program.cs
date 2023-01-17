36c9567ff66d86aab6ab6496fca486d1fe4745ac
Marina <business.voitishina@yandex.ru>
2023-01-17 16:25:54 +0300

    added homework10

---
+++ homework10/Program.cs
@@ -0,0 +1,13 @@
+﻿int secondDigit(int num)
+{
+    int firstNum = num % 100; // 56
+    int secondNum = num % 10; // 6
+    int res = (firstNum - secondNum) / 10;
+    return res;
+}
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int res = secondDigit(number);
+Console.WriteLine($"{res} ");
