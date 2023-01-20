d8c238a368cfd6d8d2ecd995a41b763a8e120997
Marina <business.voitishina@yandex.ru>
2023-01-20 11:41:33 +0300

    added task20

---
+++ task17/Program.cs
@@ -0,0 +1,27 @@
+﻿// 17. Напишите программу, которая принимает 
+// на вход координаты точки (X и Y), 
+// причём X ≠ 0 и Y ≠ 0 и 
+// выдаёт номер четверти плоскости, в которой находится эта 
+// точка.
+
+Console.WriteLine("введите координаты точки");
+Console.Write("X: ");
+int x = Convert.ToInt32(Console.Readline());
+Console.Write("Y: ");
+int y = Convert.ToInt32(Console.Readline());
+
+int quarter = Quarter(x, y);
+string result = quarter > 0 
+                ? $"Указанные координаты соответствуют четверти - {quarter}" 
+                : "Введены некорректные координаты";
+Console.WriteLine(result);
+
+//метод создаем для возвращения числа четверти
+int Quarter(int xc, int yc)
+{
+    if (xc > 0 && yc > 0) return 1;
+    if (xc < 0 && yc > 0) return 2;
+    if (xc < 0 && yc < 0) return 3;
+    if (xc > 0 && yc < 0) return 4;
+    return 0;
+}
---
+++ task18/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 18: Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.WriteLine("Задайте номер четверти: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 1 && number <= 4)
+{
+    string range = Range(number);
+    Console.WriteLine($"{range} четверти соответствуют координаты: {range}");
+}
+// else
+// {
+//     string error = Range(number):
+//     if (error == "Ошибка")
+//     {
+//         Console.WriteLine("Ошибка");
+//     }
+// }
+
+string Range(int num)
+{
+    if (num == 1) return "xc > 0, yc > 0";
+    if (num == 2) return "xc < 0, yc > 0";
+    if (num == 3) return "xc < 0, yc < 0";
+    if (num == 4) return "xc > 0, yc < 0";
+    return "ошибка";
+}
