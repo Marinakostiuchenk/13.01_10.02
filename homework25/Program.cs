0fa706c0a389a3f5bd87c402dcf1ba1a04bd91b2
Marina <business.voitishina@yandex.ru>
2023-01-24 18:49:37 +0300

    Added homework25

---
+++ homework25/Program.cs
@@ -0,0 +1,18 @@
+﻿// 25. Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+//  3, 5 -> 243 (3⁵)
+//  2, 4 -> 16
+
+Console.Write("Введите число А: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число B: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (secondNumber > 0) 
+{
+    double degree = Math.Pow(firstNumber, secondNumber);
+    Console.Write(degree);
+    Console.WriteLine(); //для красоты
+}
+Console.WriteLine("Вы ввели неверное число В");
---
+++ task30/Program.cs
@@ -0,0 +1,75 @@
+﻿
+// Варианты написания массива:
+// int[] array = new int[8]; -- так создается массив из 8 элементов (восьми нулей)
+// int[] array1 = {1,2,3};
+// int[] array = new int[8]{1,2,3,4,5,6,7,8};
+// int[] array = new int[]{1,2,3,4,5,6,7,8};
+// var array = new int[8];
+// array[0] = new Random().Next()
+
+// Задача 30: Напишите программу, которая выводит массив из 8 элементов, 
+// заполненный нулями и единицами в случайном порядке. [1,0,1,1,0,1,0,0]
+
+
+int[] array = new int[8];
+int[] FillArray(int[] arr)
+{
+    for (int i = 0; i < 8; i++)
+    {
+        arr[i] = new Random().Next(0, 2);
+    }
+    return arr;
+}
+void PrintArray(int[] arr);
+{
+    for (int i = 0; i < 8; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+}
+array = FillArray(array);
+PrintArray(array);
+
+
+// int[] GenArray(int size, int min, int max) //создание массива в методе
+// {
+//     int[] arr = new int[size];
+//     // Random rnd = new Random(); //дальше не нью рандом, а rnd.Next(min, max + 1)
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         arr[i] = new Random().Next(min, max + 1); //new Random генерирует ПСЕВДО рандом: 11111
+//     }
+//     return arr;
+// }
+
+// void PrintArray(int[] arr)
+// {
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         Console.Write($"{arr[i]} " );
+//     }
+// }
+// int [] array = GenArray(8, 0, 1);
+// PrintArray(array);
+
+
+
+// int[] GenerateArray(int size, int min, int max)
+// {
+//     int[] arr = new int[size];
+//     Random rnd = new Random();
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         arr[i] = rnd.Next(min, max + 1);
+//     }
+//     return arr;
+// }
+// void PrintArray(int[] arr)
+// {
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         Console.Write($"{arr[i]} ");
+//     }
+// }
+// int[] array = GenerateArray(8, 0, 1);
+// PrintArray(array);
