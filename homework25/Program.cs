af2e648c6fa321bca9cfc7596fee569670c24d4f
Marina <business.voitishina@yandex.ru>
2023-01-25 13:52:08 +0300

    Added homework29

--- homework25/Program.cs
+++ homework25/Program.cs
@@ -3,24 +3,6 @@
 //  3, 5 -> 243 (3⁵)
 //  2, 4 -> 16
 
-Console.Write("Введите число А: ");
-int number1 = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Введите число B: ");
-int number2 = Convert.ToInt32(Console.ReadLine());
-
-// int Degree(int num1, int num2)
-// {
-//     int count = 1;
-//     while (count != num2)
-//     {
-//        num1 *= num1;
-//        count++;
-//     }
-//     return num1;
-//     return num2;
-// }
-
 int Degree(int num1, int num2)
 {
     int degree = num1;
@@ -30,31 +12,28 @@ int Degree(int num1, int num2)
     }
     return degree; 
 }
-// {
-//     int count = 2;
 
-//     while (count < 6)
-//     {
-       
-//        num1 *= num1;
-//        count++;
-//     }
-//     return num1;
-// }
+Console.Write("Введите число А: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число B: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
 while (number2 < 1)
 {
     Console.WriteLine("Ввод неверный. Введите натуральное второе число: ");
     number2 = Convert.ToInt32(Console.ReadLine());
 }
+
 int degree = Degree(number1, number2);
 Console.WriteLine(degree);
 
 
-// if (secondNumber > 0) 
+// if (secondNumber > 1) 
 // {
 //     double degree = Math.Pow(firstNumber, secondNumber);
 //     Console.Write(degree);
-//     Console.WriteLine(); //для красоты
+//     Console.WriteLine(); 
 // }
 // Console.WriteLine("Вы ввели неверное число В");
 
