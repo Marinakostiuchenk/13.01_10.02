ed3bbb7997c2781dfa0db7543a8bb8cdc7a8e99c
Marina <business.voitishina@yandex.ru>
2023-01-27 17:13:03 +0300

    Added homework34

---
+++ homework34/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] FillArray(int size, int min, int max) 
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("] ");
+}
+
+int Event(int[] array)
+{
+int num = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0) 
+    {
+        num += i;
+    }
+}
+return num;
+}
+
+int[] array = FillArray(8, 100, 999);
+PrintArray(array); 
+
+int res = Event(array);
+Console.Write(res);
