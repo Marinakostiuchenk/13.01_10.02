aa76fe9334f3a7c2105d818853049137439ca9da
Marina <business.voitishina@yandex.ru>
2023-01-31 11:39:43 +0300

    task 42

--- task40/Program.cs
+++ task40/Program.cs
@@ -5,8 +5,7 @@
 
 bool Triangle(int num1, int num2, int num3)
 {
-    if (num1 < num2 + num3 && num2 < num1 + num3 && num3 < num1 + num2) return true;
-    else return false;
+    return num1 < num2 + num3 && num2 < num1 + num3 && num3 < num1 + num2; 
 }
 
 Console.WriteLine("Введите 3 числа. Первое число: ");
@@ -17,4 +16,4 @@ Console.WriteLine("Третье число: ");
 int number3 = Convert.ToInt32(Console.ReadLine());
 
 bool triangle = Triangle(number1, number2, number3);
-Console.WriteLine(triangle);
+Console.WriteLine(triangle ? "Да" : "Нет"); //вместо в методе: if и else
--- task42/Program.cs
+++ task42/Program.cs
@@ -1,3 +1,43 @@
 ﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное. 
 // 45 -> 101101 3 -> 11 2 -> 10
 
+Console.WriteLine("Введите десятичное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// string Recursia(int num)
+// {
+//     string i = string.Empty;
+
+//     while (num > 0)
+//     {
+//         if (num % 2 == 0)
+//         {
+//             i = 0;
+//             num = num / 2;
+//         }
+//         else 
+//         {
+//             i = 1;
+//             num = num / 2;
+//         }
+//     }
+//     return i;
+// }
+
+// string res  = Recursia(number);
+// Console.WriteLine(res);
+
+
+string InverseNumber(int num)
+{
+    string res = string.Empty;
+    while (num > 0)
+    {
+        res = num % 2 + res;
+        num /= 2;
+        
+    }
+    return res;
+}
+string inverseNumber = InverseNumber(number);
+Console.WriteLine(inverseNumber);
