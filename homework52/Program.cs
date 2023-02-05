b884fa0cdd9cc9cbfe834271b76bd436bcfd2cd3
Marina <business.voitishina@yandex.ru>
2023-02-05 19:53:51 +0300

    task 52

--- homework52/Program.cs
+++ homework52/Program.cs
@@ -1,33 +1,54 @@
-﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
 // Например, задан массив:
 // 1 4 7 2
 // 5 9 2 3
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
-int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+double[,] CreateMatrixRndInt(int rows, int columns)
 {
-    int[,] matrix = new int[rows, columns]; // 0, 1 
+    double[,] matrix = new double[rows, columns]; // 0, 1 
     Random rnd = new Random();
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            matrix[i, j] = rnd.Next(min, max + 1);
+            matrix[i, j] = rnd.Next(0, 10);
         }
     }
     return matrix;
 }
 
-void PrintMatrix(int[,] matrix)
+void PrintMatrix(double[,] matrix)
 {
-    for (int i = 0; i < matrix.GetLength(0); i++)
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
     {
-        Console.Write("|"); for (int j = 0; j < matrix.GetLength(1); j++)
+        Console.Write(string.Format("{0,5}", matrix[i, j] + " "));
+    }
+    Console.WriteLine();
+}
+}
+
+void Average(double[,] matrix)
+{
+    double average = 0;
+
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
         {
-            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],3} | ");
-            else Console.Write($"{matrix[i, j],3} ");
+            average += matrix[i, j] / matrix.GetLength(0);
         }
-        Console.WriteLine("|");
+        Console.WriteLine(string.Format ("Средняя арифметическая в столбце {0} = {1}", j, Math.Round(average, 1)));
+        average = 0;
     }
 }
+
+
+double[,] matr = CreateMatrixRndInt(3, 4);
+PrintMatrix(matr);
+Average(matr);
