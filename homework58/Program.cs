d00cc80a2706b2fef66883e3086c73a8502d0445
Marina <business.voitishina@yandex.ru>
2023-02-11 20:09:26 +0300

    task 58

--- homework58/Program.cs
+++ homework58/Program.cs
@@ -7,3 +7,74 @@
 // 18 20
 // 15 18
 
+int[,] CreateMatrixA(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns]; // 0, 1 
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+int[,] CreateMatrixB(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns]; // 0, 1 
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+
+
+void MultiplyMatrix(int[,] matrixA, int[,] matrixB)
+{
+    int[,] multyply = new int[matrixA.GetLength(0), matrixB.GetLength(1)];
+
+    for (int i = 0; i < matrixA.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixB.GetLength(1); j++)
+        {
+            multyply[i, j] = 0;
+            for (int q = 0; q < matrixA.GetLength(1); q++)
+            {
+                multyply[i, j] += matrixA[i, q] * matrixB[q, j];
+            }
+            Console.Write(multyply[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|"); for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5} | ");
+            else Console.Write($"{matrix[i, j],5} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int[,] matr1 = CreateMatrixA(2, 2, 0, 10);
+int[,] matr2 = CreateMatrixB(2, 2, 0, 10);
+PrintMatrix(matr1);
+Console.WriteLine();
+PrintMatrix(matr2);
+Console.WriteLine();
+MultiplyMatrix(matr1, matr2);
